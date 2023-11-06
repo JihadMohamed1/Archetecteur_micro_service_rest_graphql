e80d6f67897d7b9960900bfdd95c985e8c705e30
Mohamed jihad <84962456+JihadMohamed1@users.noreply.github.com>
2023-11-06 09:38:45 +0100

    Update script.groovy

--- MCS1/script.groovy
+++ MCS1/script.groovy
@@ -1,8 +1,20 @@
-def buildApp(){
-  echo 'build stage'
+def buildJar(){
+  echo 'build jar stage'
+  sh 'mvn package'
 }
-def testApp(){
-  echo 'test stage'
+def buildImage(){
+  echo 'build Image stage'
+  whithCredentials(
+    [usernamePassword(
+      credentialsId : 'dockerhub_cred',
+      passwordVariable : PASS,
+      usernameVariable : USER
+    )]
+  ){
+    sh 'docker build -t 67.205.176.30:8083:jma-2.0'
+    sh "echo $PASS | docker login -u $USER --password-stdin 67.205.176.30:8083"
+    sh 'docker push 67.205.176.30:8083:jma-2.0'
+  }
 }
 def deployApp(){
   echo 'deploy stage'
